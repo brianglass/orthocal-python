603dcb00d051b19dd278d0b32a2b51a20521c161
Brian Glass <bglass@remmac06337.local>
2023-01-07 20:42:06 -0500

    Add forgotten files

---
+++ calendarium/tests/test_api.py
@@ -0,0 +1,52 @@
+import json
+
+from pathlib import Path
+
+from django.urls import reverse
+from django.utils import timezone
+from rest_framework.test import APITestCase
+
+# Build paths inside the project like this: BASE_DIR / 'subdir'.
+BASE_DIR = Path(__file__).resolve().parent
+
+
+class DayAPITestCase(APITestCase):
+    fixtures = ['calendarium.json']
+
+    def test_get_day(self):
+        with open(BASE_DIR / 'data/last_bday.json') as f:
+            expected = json.loads(f.read())
+
+        url = reverse('day-get', kwargs={
+            'jurisdiction': 'oca',
+            'year': 2022,
+            'month': 1,
+            'day': 7,
+        })
+        response = self.client.get(url, format='json')
+        actual = response.json()
+
+        self.assertEqual(expected, actual)
+
+    def test_get_day_default(self):
+        url = reverse('day-get-default', kwargs={'jurisdiction': 'oca'})
+        response = self.client.get(url, format='json')
+        dt = timezone.localtime()
+        actual = response.json()
+        self.assertEqual(dt.year, actual['year'])
+        self.assertEqual(dt.month, actual['month'])
+        self.assertEqual(dt.day, actual['day'])
+
+    def test_list_days(self):
+        with open(BASE_DIR / 'data/january.json') as f:
+            expected = json.loads(f.read())
+
+        url = reverse('day-list', kwargs={
+            'jurisdiction': 'oca',
+            'year': 2022,
+            'month': 1,
+        })
+        response = self.client.get(url, format='json')
+        actual = response.json()
+
+        self.assertEqual(expected, actual)
