6530d57da4cc50aed88aaae50b829fbd1d5a21b0
Brian Glass <bglass@remmac06337.local>
2023-01-11 09:52:02 -0500

    Add Day intent handler to skill

--- calendarium/skills.py
+++ calendarium/skills.py
@@ -11,6 +11,7 @@ skill_builder = SkillBuilder()
 
 # These handlers don't seem to support async
 
+
 class OrthodoxDailyLaunchHandler(AbstractRequestHandler):
     def can_handle(self, handler_input):
         return is_request_type('LaunchRequest')(handler_input)
@@ -31,6 +32,34 @@ class OrthodoxDailyLaunchHandler(AbstractRequestHandler):
         return builder.response
 
 
+class DayIntentHandler(AbstractRequestHandler):
+    def can_handle(self, handler_input):
+        return is_intent_name('Day')(handler_input)
+
+    def handle(self, handler_input):
+        builder = handler_input.response_builder
+        attributes = handler_input.attributes_manager.session_atttributes
+
+        if date_text := attributes.get('date'):
+            date = datetime.strptime('%Y-%m-%d', date_text)
+            day = liturgics.Day(date.year, date.month, date.day)
+        else:
+            today = timezone.localtime()
+            day = liturgics.Day(today.year, today.month, today.day)
+
+        day.initialize()
+
+        speech_text, card_text = speech.day_speech(day)
+        builder.speak(speech_text)
+
+        when = speech.when_speech(day)
+        card = SimpleCard(f'About {when}', card_text)
+        builder.set_card(card)
+
+        return builder.response
+
+
 skill_builder.add_request_handler(OrthodoxDailyLaunchHandler())
+skill_builder.add_request_handler(DayIntentHandler())
 
 orthodox_daily_skill = skill_builder.create()
