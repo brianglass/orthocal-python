1aabdca2608a3a9d10d85dacb22729c3cdd52e5f
Brian Glass <bglass@remmac06337.local>
2023-01-13 12:20:55 -0500

    Add forgotten files to repo

---
+++ alexa/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class AlexaConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'alexa'
---
+++ alexa/urls.py
@@ -0,0 +1,13 @@
+from django.urls import path
+from django_ask_sdk.skill_adapter import SkillAdapter
+from rest_framework.urlpatterns import format_suffix_patterns
+
+from . import skills
+
+orthodox_daily_view = SkillAdapter.as_view(skill=skills.orthodox_daily_skill)
+
+urlpatterns = [
+    path('alexa/', orthodox_daily_view),
+]
+
+urlpatterns = format_suffix_patterns(urlpatterns)
