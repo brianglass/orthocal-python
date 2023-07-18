41f94eeecbe3eba4ceecf3519cca1cad21c8d32c
Brian Glass <brian@glassbrian.com>
2023-07-18 14:56:11 -0400

    Namespace hub link in RSS

--- calendarium/websub.py
+++ calendarium/websub.py
@@ -6,7 +6,7 @@ from django.utils.feedgenerator import Rss201rev2Feed
 class WSRssFeed(Rss201rev2Feed):
     def add_root_elements(self, handler):
         super().add_root_elements(handler)
-        handler.addQuickElement('link', '', {
+        handler.addQuickElement('atom:link', '', {
             'rel': 'hub',
             'href': settings.ORTHOCAL_WEBSUB_URL,
         })
