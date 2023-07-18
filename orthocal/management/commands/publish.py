e25dda3851c068244fa26d60456cd7f490e37c7c
Brian Glass <brian@glassbrian.com>
2023-07-18 13:00:05 -0400

    Log results on success as well as failure

--- orthocal/management/commands/publish.py
+++ orthocal/management/commands/publish.py
@@ -28,3 +28,5 @@ class Command(BaseCommand):
 
         if not response.ok:
             logger.error(f'WEBSUB Publish error ({response.status_code}): {response.text}')
+        else:
+            logger.info(f'WEBSUB Publish success ({response.status_code}): {response.text}')
