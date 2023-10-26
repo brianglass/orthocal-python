32edd9bedece22cff942bb84489580796f88ec8e
Brian Glass <brian@glassbrian.com>
2023-10-26 19:30:28 -0400

    Add Fastly cache control headers

--- orthocal/middleware.py
+++ orthocal/middleware.py
@@ -12,18 +12,19 @@ def cache_control(get_response):
     now = timezone.localtime()
     midnight = now.replace(hour=0, minute=0, second=0, microsecond=0) + datetime.timedelta(days=1)
     max_age = int((midnight - now).total_seconds())
-    print(max_age)
 
     if asyncio.iscoroutinefunction(get_response):
         async def middleware(request):
             response = await get_response(request)
-            patch_cache_control(response, public=True, max_age=max_age)
+            response['Surrogate-Control'] = f'max-age={max_age}'
+            patch_cache_control(response, public=True, max_age=settings.ORTHOCAL_MAX_AGE)
             patch_vary_headers(response, settings.ORTHOCAL_VARY_HEADERS)
             return response
     else:
         def middleware(request):
             response = get_response(request)
-            patch_cache_control(response, public=True, max_age=max_age)
+            response['Surrogate-Control'] = f'max-age={max_age}'
+            patch_cache_control(response, public=True, max_age=settings.ORTHOCAL_MAX_AGE)
             patch_vary_headers(response, settings.ORTHOCAL_VARY_HEADERS)
             return response
 
