0862e9a6bcf269157684d00727cfd9fa74598ff9
Brian Glass <brian@glassbrian.com>
2023-10-26 19:41:02 -0400

    Change the way CDN cache control is set

--- orthocal/middleware.py
+++ orthocal/middleware.py
@@ -8,23 +8,21 @@ from django.utils.decorators import sync_and_async_middleware
 
 @sync_and_async_middleware
 def cache_control(get_response):
-    # Everything expires at midnight
+    # Everything expires at midnight for the CDN. s-maxage is for Firebase CDN.
     now = timezone.localtime()
     midnight = now.replace(hour=0, minute=0, second=0, microsecond=0) + datetime.timedelta(days=1)
-    max_age = int((midnight - now).total_seconds())
+    cdn_max_age = int((midnight - now).total_seconds())
 
     if asyncio.iscoroutinefunction(get_response):
         async def middleware(request):
             response = await get_response(request)
-            response['Surrogate-Control'] = f'max-age={max_age}'
-            patch_cache_control(response, public=True, max_age=settings.ORTHOCAL_MAX_AGE)
+            patch_cache_control(response, public=True, max_age=settings.ORTHOCAL_MAX_AGE, s_maxage=cdn_max_age)
             patch_vary_headers(response, settings.ORTHOCAL_VARY_HEADERS)
             return response
     else:
         def middleware(request):
             response = get_response(request)
-            response['Surrogate-Control'] = f'max-age={max_age}'
-            patch_cache_control(response, public=True, max_age=settings.ORTHOCAL_MAX_AGE)
+            patch_cache_control(response, public=True, max_age=settings.ORTHOCAL_MAX_AGE, s_maxage=cdn_max_age)
             patch_vary_headers(response, settings.ORTHOCAL_VARY_HEADERS)
             return response
 
