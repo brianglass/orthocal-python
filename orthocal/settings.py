d51672027f0304cf0a70d267283e0bb175b75236
Brian Glass <brian@glassbrian.com>
2023-12-25 01:28:32 -0500

    Reduce cache MAX_ENTRIES

--- orthocal/settings.py
+++ orthocal/settings.py
@@ -145,7 +145,7 @@ CACHES = {
         'TIMEOUT': ORTHOCAL_MAX_AGE,
         'LOCATION': BASE_DIR / 'default-cache',
         'OPTIONS': {
-            'MAX_ENTRIES': 1500,
+            'MAX_ENTRIES': 1000,
         },
     },
 }
